f05f9f257a3433678983083cdde7c95f609d1147
KimYeonWoo <zxasqw155@naver.com>
2021-09-02 23:27:43 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const { User } = require('./models/User')
+
+const config = require('./config/key')
+
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}))
+//application/json
+app.use(express.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yeonwoo:abcd1234@boilerplate.nqgoq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology:true
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! yeonwoo world!')
+})
+
+app.post('/register', (req,res) => {
+
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if(err) return res.json({ success : false, err})
+    return res.status(200).json({
+      success : true
+    })
+  })
 })
 
 app.listen(port, () => {
